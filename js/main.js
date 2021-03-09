81f949321fe6e22ce6aaad8f32080dc0b398ba23
Danielle Martinez <daniellealexandria10@gmail.com>
2021-03-08 18:54:23 -0700

    Countdown guesses

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ let randomWord = random
 let answerArr = [];
 let wrong = [];
 answerArr = new Array(randomWord.length).fill('_');
+let maxGuesses = 6;
 
 
 /*----- cached element references -----*/
@@ -16,10 +17,12 @@ const submit = document.getElementById('submit');
 const msg = document.getElementById('msg');
 const displayResults = document.querySelector('h1');
 const inp = document.querySelector('input');
+const guesses = document.querySelector('nav');
 
 /*----- event listeners -----*/
 reset.addEventListener('click', init);
 submit.addEventListener('click', submitGuess);
+document.addEventListener('keydown', init);
 
 /*----- functions -----*/
 
@@ -30,6 +33,7 @@ function init() {
     let wrong = []; 
     let answerArr = [];
     let randomWord = random;
+    guesses.textContent = `Guesses remaining: ${maxGuesses}`;
     render(); 
 };
 
@@ -49,6 +53,8 @@ function render() {
     } else if(!randomWord.includes(lowCaseInp)) {
         wrong.push(lowCaseInp);
         msg.textContent = 'Wrong!';
+        maxGuesses--;
+        guesses.textContent = `Guesses remaining: ${maxGuesses}`;
     } else {
         msg.textContent = `Invalid, Please enter a letter`;
     }
@@ -75,10 +81,10 @@ function submitGuess() {
 
 function getWinner() {
    
-    if ((!answerArr.includes('_')) && (wrong.length <= 6)) {
+    if ((!answerArr.includes('_')) && (wrong.length <= 5)) {
         msg.textContent = `You win the answer was `
         reset.style.visibility = 'visible';
-    } else if (answerArr.includes('_') && (wrong.length > 6)) { 
+    } else if (answerArr.includes('_') && (wrong.length > 5)) { 
         msg.textContent = `Oh no you ran out of guesses. The correct answer was ${randomWord}`;
         reset.style.visibility = 'visible';
         } 
